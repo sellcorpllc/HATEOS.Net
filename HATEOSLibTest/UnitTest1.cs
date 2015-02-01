94998c273f774a9299b6998a135923335af4a430
Robert Sell <sellcorp.llc@gmail.com>
2015-01-31 21:25:48 -0500

    Added unit test to extract the resource type contained in the resource attribute, and the value of a property with the resource key attribute.

--- HATEOSLibTest/UnitTest1.cs
+++ HATEOSLibTest/UnitTest1.cs
@@ -3,6 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AttributeTestLib;
 using HATEOS_Lib.Attributes;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Data.Linq;
+using System.Linq;
+
 
 namespace HATEOSLibTest
 {
@@ -10,7 +14,7 @@ namespace HATEOSLibTest
     public class UnitTest1
     {
         [TestMethod]
-        public void ResourceAttributeTest()
+        public void ResourceAttributeItemTest()
         {        
             //Get a list
             List<Attribute> attrs = new List<Attribute>(Attribute.GetCustomAttributes(typeof(Item)));
@@ -26,6 +30,65 @@ namespace HATEOSLibTest
             Assert.IsTrue(resourceName.CompareTo("item")==0);
         }
 
-        public void Resou
+        [TestMethod]
+        public void ResourceAttributeItemForObjectTest()
+        {
+            Item testItem = new Item();
+            testItem.ItemId = 4;
+            testItem.Name = "Test item";
+
+            string resourceName = "";
+
+            Resource r = testItem.GetType().GetCustomAttribute<Resource>();
+            resourceName = r.resourceName;
+            Assert.IsTrue(resourceName.CompareTo("item") == 0);
+        }
+
+
+        [TestMethod]
+        public void ResourceKeyTest()
+        {
+            Item testItem = new Item();
+            testItem.ItemId = 4;
+            testItem.Name = "Test item";
+
+            //Get the propertylist for the item
+            List<PropertyInfo> propertyList = new List<PropertyInfo>(testItem.GetType().GetProperties());
+
+            //Generate a list of properties where an attribute of ResourceKey is defined for the property and take the first 
+            PropertyInfo p = propertyList.Where(prop => Attribute.IsDefined(prop, typeof(ResourceKey))).FirstOrDefault();
+
+            // Couple of different way to get the value of the property that has a resource key attribute
+            string valueOfResourceKey1 = p.GetValue(testItem, null).ToString();
+            string valueOfResourceKey2 =  testItem.GetType().GetProperty(p.Name).GetValue(testItem, null).ToString();
+           
+            
+            //The ultimate statement of enlightenment I have been searching for
+            //Get the type for the item, then properters which have an attirbute of resource key, take the first and only one and then get its value for the object of the item
+            string everythingTogether = testItem.GetType().GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(ResourceKey))).FirstOrDefault().GetValue(testItem, null).ToString();
+
+            //This ran in at 5ms. Which might be to slow for production purposes and need explored later.
+            Assert.IsTrue(everythingTogether.CompareTo("4")==0);
+           
+        }
+
+
+        [TestMethod]
+        public void urlTestForTestClass()
+        {
+            string baseUrl = @"/api";
+            string expectEdUrl = @"/api/item/4";
+
+            Item testItem = new Item();
+            testItem.ItemId = 4;
+            testItem.Name = "Test item";
+
+            string resourceName = testItem.GetType().GetCustomAttribute<Resource>().resourceName;
+            string resourceKey = testItem.GetType().GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(ResourceKey))).FirstOrDefault().GetValue(testItem, null).ToString();
+
+            string testUrl = String.Format(baseUrl + "/{0}/{1}", resourceName, resourceKey);
+
+            Assert.IsTrue(expectEdUrl.CompareTo(testUrl) == 0);
+        }
     }
 }
