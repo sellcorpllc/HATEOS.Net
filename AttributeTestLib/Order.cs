765b3958f7af9d19e5bba07212dca09c2345227f
rnsell <sellcoorp.llc@gmail.com>
2015-02-01 13:30:51 -0500

    Added Order class.

---
+++ AttributeTestLib/Order.cs
@@ -0,0 +1,35 @@
+﻿using HATEOS_Lib.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeTestLib
+{
+    [Resource("order")]
+    public class Order
+    {
+        private int _orderId;
+        List<OrderLine> _orderLines;
+
+        [ResourceKey()]
+        public int OrderId
+        {
+            get { return _orderId; }
+            set { _orderId = value; }
+        }
+
+        [RelatedResource("field")]
+        public List<OrderLine> OrderLines
+        {
+            get { return _orderLines; }
+            set { _orderLines = value; }
+        }
+
+
+
+
+
+    }
+}
